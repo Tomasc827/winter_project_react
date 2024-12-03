44359e51d578789d904b1dcd04f810d25170c4f4
Tomas <tomas.ceponis@stud.techin.lt>
2024-12-03 20:33:02 +0200

    minor layout fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router'
 import './App.css'
 
 function App() {
@@ -5,9 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ createRoot(document.getElementById("root")).render(
           <Route path="/" element={<App />}>
             <Route index element={<LoginPage />} />
           </Route>
-          <App />
         </Routes>
       </DataProviders>
     </BrowserRouter>
