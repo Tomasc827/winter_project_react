3dc4caee7e75123c20a36e45c6d96fcc5bc831ea
Tomas <tomas.ceponis@stud.techin.lt>
2024-12-04 22:51:53 +0200

    form input start

--- src/assets/components/SignUpPage.jsx
+++ src/assets/components/SignUpPage.jsx
@@ -1,11 +1,17 @@
-import Logo from "../svg/logo.svg"
+import Logo from "./formatted_svg/Logo";
 
 const SignUpPage = () => {
     return ( 
         <>
-        <div className="desktop:mt-[4.9rem] desktop:mb-[15.62rem] desktop:min-w-[90rem]">
-            <div>
-                
+        <div className="desktop:pt-[4.9rem] desktop:pb-[15.62rem] desktop:max-w-[90rem]">
+            <div className="flex justify-center desktop:mb-[5.19rem]">
+               <Logo/> 
+            </div>
+            <div className="desktop:flex desktop:justify-center">
+            <form className="desktop:w-[25rem] desktop:h-[26.125rem] bg-figma-semi-dark-blue rounded-[1.25rem] p-[2rem]">
+            <h2 className="figma-heading-l text-white pb-[2.5rem]">Sign up</h2>
+            <input className="tablet:w-[21rem] tablet:h-[2.3125rem] input-login-style" placeholder="Email address"></input>
+            </form>
             </div>
 
         </div>
