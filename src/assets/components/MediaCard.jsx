d3e757077637213b6da54b3172c0b2d0a0b26711
lukas622 <bsds356@proton.me>
2024-12-06 15:23:38 +0200

    Play button adjustments

--- src/assets/components/MediaCard.jsx
+++ src/assets/components/MediaCard.jsx
@@ -1,4 +1,5 @@
 import BookmarkButton from "./BookmarkButton";
+import "./MediaCard.css";
 const MediaCard = (props) => {
   let categoryIcon = "src/assets/svg/icon-category-tv.svg";
 
@@ -7,17 +8,33 @@ const MediaCard = (props) => {
   }
   return (
     <div>
-      <div className="relative group">
+      <div className="relative group ">
         <img
-          className="rounded-lg group-hover:blur-sm"
+          className="rounded-lg group-hover:blur-sm duration-500 "
           src={props.thumbnail}
           alt=""
         />
-        <img
-          className="absolute hidden pointer-events-none group-hover:block top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[30%]  "
-          src="src/assets/svg/icon-play.svg"
-          alt=""
-        />
+
+        <div className="absolute hidden pointer-events-none group-hover:block duration-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[30%] h-[20%]">
+          <div className="opacity-25 absolute rounded-full bg-white w-full h-full"></div>
+          <div className="flex flex-row h-full justify-between justify-items-center text-center items-center">
+            <img
+              className="w-[30%] ml-3"
+              src="src/assets/svg/icon-play.svg"
+              alt=""
+            />
+            <p className="mr-[30%] text-white font-semibold">Play</p>
+          </div>
+          {/* <img
+            className="absolute top-1/2 left-4 transform -translate-y-1/2 w-[30%]"
+            src="src/assets/svg/icon-play.svg"
+            alt=""
+          />
+          <p className="absolute top-1/2 left-[50%] transform -translate-y-1/2 text-white text-2xl font-semibold">
+            Play
+          </p> */}
+        </div>
+
         <BookmarkButton
           media_id={props.media_id}
           isBookmarked={props.isBookmarked}
