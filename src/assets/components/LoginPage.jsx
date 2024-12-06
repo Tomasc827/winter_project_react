22995e325158f28ee986d8fdf3cb9064f203a6c6
TomasC <tomas.ceponis@stud.techin.lt>
2024-12-06 14:07:56 +0400

    login functionality and css add

--- src/assets/components/LoginPage.jsx
+++ src/assets/components/LoginPage.jsx
@@ -31,25 +31,26 @@ const LoginPage = () => {
     <>
       <ErrorServer />
       <SignUpSuccess/>
-      <div className="desktop:pt-[4.9rem] desktop:pb-[12.81rem] desktop:min-w-[90rem] tablet:pt-[5.5rem] tablet:pb-[26.25rem] tablet:px-[11.5rem] phone:pt-[3rem] phone:pb-[7.19rem] phone:px-[1.5rem]">
+      <div className="desktop:pt-[4.9rem] desktop:pb-[15.62rem] desktop:min-w-[90rem] tablet:pt-[5rem] tablet:pb-[29.56rem] tablet:px-[11.5rem] phone:pt-[3rem] phone:pb-[7.19rem] phone:px-[1.5rem]">
         <div className="flex justify-center desktop:mb-[5.19rem] tablet:pb-[4.53rem] phone:pb-[3.65rem]">
           <Logo />
         </div>
         <div className="phone:flex phone:justify-center">
           <form
-            className="tablet:min-w-[25rem] tablet:min-h-[26.125rem]  bg-figma-semi-dark-blue rounded-[1.25rem] tablet:p-[2rem] phone:p-[1.5rem] phone:flex phone:flex-col phone:min-w-[20.4375rem] phone:min-h-[26.25rem]"
+            className="tablet:min-w-[25rem] tablet:min-h-[23.3125rem]  bg-figma-semi-dark-blue rounded-[1.25rem] tablet:p-[2rem] phone:p-[1.5rem] phone:flex phone:flex-col phone:min-w-[20.4375rem] phone:min-h-[22.8125rem]"
             onSubmit={handleSubmit(onSubmit)}
             
           >
             <h2 className="figma-heading-l text-white pb-[2.5rem]">Login</h2>
             <div className="relative">
               <input
-                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2  ${
+                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red  ${
                   errors.email
-                    ? "focus:border-figma-red border-figma-red caret-figma-red "
+                    ? "focus:border-figma-red border-figma-red"
                     : "focus:border-figma-white"
                 }`}
                 placeholder="Email address"
+                
                 type="text"
                 id="email"
                 {...register("email", {
@@ -69,9 +70,9 @@ const LoginPage = () => {
             </div>
             <div className="relative">
               <input
-                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2  ${
+                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[2.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red  ${
                   errors.password
-                    ? "focus:border-figma-red border-figma-red caret-figma-red"
+                    ? "focus:border-figma-red border-figma-red"
                     : "focus:border-figma-white"
                 }`}
                 placeholder="Password"
@@ -105,10 +106,10 @@ const LoginPage = () => {
               type="submit"
               value="submit"
             >
-              Create an account
+              Login to your account
             </button>
-            <p className="figma-body-m text-figma-white tablet:ps-[3.81rem] phone:ps-[2.19rem]">
-              Already have an account?
+            <p className="figma-body-m text-figma-white  tablet:ps-[3.75rem] phone:ps-[2.19rem]">
+              Don't have an account?
               <span className="ps-[0.5rem] text-figma-red cursor-pointer" onClick={() => navigate("/signup")}>Sign up</span>
             </p>
           </form>
