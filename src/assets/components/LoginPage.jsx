72d59f682ae6512386b3ad82a9b2e366192b379c
lukas622 <bsds356@proton.me>
2024-12-06 12:43:06 +0200

    Fix autofill backround problem

--- src/assets/components/LoginPage.jsx
+++ src/assets/components/LoginPage.jsx
@@ -6,31 +6,35 @@ import SignUpSuccess from "./SignUpSuccess";
 import { loginUser } from "../helpers/getUser";
 
 const LoginPage = () => {
-  const {setError, navigate, setSuccess,error,processing, setCurrentUser } = useData();
+  const { setError, navigate, setSuccess, error, processing, setCurrentUser } =
+    useData();
 
-    const{register, formState: {errors}, handleSubmit} = useForm()
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-    const onSubmit = async (data) => {
-        if (error || processing) return
-    
+  const onSubmit = async (data) => {
+    if (error || processing) return;
+
     try {
-        const user = await loginUser(data)
-        setCurrentUser(user)
-        setSuccess("Login successful")
-        setTimeout(() => {
-            navigate("/homepage")
-            setSuccess("")
-        },2500)
-        
+      const user = await loginUser(data);
+      setCurrentUser(user);
+      setSuccess("Login successful");
+      setTimeout(() => {
+        navigate("/homepage");
+        setSuccess("");
+      }, 2500);
     } catch (error) {
-        setError(error.message)
+      setError(error.message);
     }
-}
+  };
 
   return (
     <>
       <ErrorServer />
-      <SignUpSuccess/>
+      <SignUpSuccess />
       <div className="desktop:pt-[4.9rem] desktop:pb-[15.62rem] desktop:min-w-[90rem] tablet:pt-[5rem] tablet:pb-[29.56rem] tablet:px-[11.5rem] phone:pt-[3rem] phone:pb-[7.19rem] phone:px-[1.5rem]">
         <div className="flex justify-center desktop:mb-[5.19rem] tablet:pb-[4.53rem] phone:pb-[3.65rem]">
           <Logo />
@@ -39,18 +43,16 @@ const LoginPage = () => {
           <form
             className="tablet:min-w-[25rem] tablet:min-h-[23.3125rem]  bg-figma-semi-dark-blue rounded-[1.25rem] tablet:p-[2rem] phone:p-[1.5rem] phone:flex phone:flex-col phone:min-w-[20.4375rem] phone:min-h-[22.8125rem]"
             onSubmit={handleSubmit(onSubmit)}
-            
           >
             <h2 className="figma-heading-l text-white pb-[2.5rem]">Login</h2>
             <div className="relative">
               <input
-                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red  ${
+                className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red autofill:transition-colors autofill:duration-[5000000ms]  ${
                   errors.email
                     ? "focus:border-figma-red border-figma-red"
                     : "focus:border-figma-white"
                 }`}
                 placeholder="Email address"
-                
                 type="text"
                 id="email"
                 {...register("email", {
@@ -110,7 +112,12 @@ const LoginPage = () => {
             </button>
             <p className="figma-body-m text-figma-white  tablet:ps-[3.75rem] phone:ps-[2.19rem]">
               Don't have an account?
-              <span className="ps-[0.5rem] text-figma-red cursor-pointer" onClick={() => navigate("/signup")}>Sign up</span>
+              <span
+                className="ps-[0.5rem] text-figma-red cursor-pointer"
+                onClick={() => navigate("/signup")}
+              >
+                Sign up
+              </span>
             </p>
           </form>
         </div>
