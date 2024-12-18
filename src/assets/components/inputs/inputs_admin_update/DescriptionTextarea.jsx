ba87df625affc974a9ef07022278cba733f5a07f
Sasha <apigusova64@gmail.com>
2024-12-18 10:37:34 +0200

    Adding css for admins update modal

--- src/assets/components/inputs/inputs_admin_update/DescriptionTextarea.jsx
+++ src/assets/components/inputs/inputs_admin_update/DescriptionTextarea.jsx
@@ -5,7 +5,7 @@ const DescriptionTextArea = ({register,errors}) => {
         <textarea
         rows="7"
         cols="40"
-          className={`tablet:w-[21rem] h-[2.3125rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red ${
+          className={`desktop:w-[40rem] tablet:w-[35rem] h-[5rem] phone:w-[17.4375rem] input-login-style mb-[1.5rem] text-white figma-body-m focus:border-b-2 caret-figma-red ${
             errors.description
               ? "focus:border-figma-red border-figma-red"
               : "focus:border-figma-white"
