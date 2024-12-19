60d34c9234d593201c2099ec7aa4851851cadfe7
Sasha <apigusova64@gmail.com>
2024-12-19 09:59:50 +0200

    Adding fixed plus sign positioning

--- src/assets/components/cards/Description.jsx
+++ src/assets/components/cards/Description.jsx
@@ -6,7 +6,7 @@ import CloseDescription from "../formatted_svg/CloseDescription";
 import RatingsButton from "../RatingsButton";
 import AnimationRouteModal from "../messages/AnimationRouteModal";
 
-const Description = ({handleClose}) => {
+const Description = ({ handleClose }) => {
   const { findShowById, navigate, fetchData, onLoginCheck } = useData();
 
   const { showID } = useParams();
@@ -27,96 +27,99 @@ const Description = ({handleClose}) => {
     );
   }
 
-
-
   return (
     <>
       {show && (
         <AnimationRouteModal>
-          {({onClose}) => ( 
-          <div className="relative">
-            <div className="fixed inset-0 bg-black z-40 bg-opacity-50 flex justify-center items-center">
-              <div
-                className="desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] "
-              >
-                <div className="bg-figma-semi-dark-blue text-figma-white rounded-t-3xl rounded-b-3xl rounded shadow-2xl shadow-figma-red ">
-                  <div className="relative">
-                    <img
-                      src={
-                        show.thumbnail.regular.large.startsWith("/")
-                          ? show.thumbnail.regular.large
-                          : `/${show.thumbnail.regular.large}`
-                      }
-                      alt={show.title}
-                      className="desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] h-auto object-contain rounded-t-3xl border-2 "
-                    ></img>
-                    <div className="absolute top-2 left-20">
-                      <BookmarkButton
-                        media_id={show.id}
-                        isBookmarked={show.isBookmarked}
-                        reloadData={fetchData}
-                      />
-                    </div>
-                    <div className="absolute top-[1.60rem] right-8" onClick={onClose}>
-                      <CloseDescription />
-                    </div>
-                  </div>
-                  <p className="text-center figma-heading-l border-x-2 pt-[1rem]">
-                    {show.title}
-                  </p>
-                  <div className="grid grid-cols-3 place-items-center desktop:px-[3rem] tablet:px-[2rem]  phone:px-[1rem] figma-heading-m pt-[1rem] pb-[1rem] border-x-2 w-[30vw] desktop:w-[40rem] tablet:min-w-[30rem]  phone:min-w-[20rem]">
-                    <div className="flex flex-col text-center desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] ">
-                      <p className="figma-body-s  phone:text-[0.5em]">
-                        Release Year:
-                      </p>
-                      <p>{show.year}</p>
-                    </div>
-                    <div className="flex flex-col text-center ">
-                      <p className="figma-body-s phone:text-[0.5em]">Category:</p>
-                      <p className="">
-                        {show.category === "TV Series" ? "Series" : "Movie"}
-                      </p>
-                    </div>
-                    <div className="flex flex-col text-center">
-                      <p className="figma-body-s phone:text-[0.5em]">Rating:</p>
-                      <p>{show.rating}</p>
-                    </div>
-                  </div>
-                  <div className="flex justify-center pb-3 border-x-2">
-                    <RatingsButton
-                      showID={showID}
-                      contentId={show.id}
-                      averageRating={show.averageRating}
-                      totalRatings={show.totalRatings}
-                      userRating={show.userRating}
-                    />
-                  </div>
-                  <div className="flex flex-col text-center desktop:w-[40rem] tablet:min-w-[30rem]  phone:min-w-[20rem] w-[30vw] px-[2rem] figma-body-m border-x-2 border-b-2 rounded-b-3xl">
-                    <p>Synopsis:</p>
-                    <p className="">{show.description}</p>
-                    <div className="flex pt-[3rem] justify-between px-[3rem] gap-x-[3rem] tablet:flex-row phone:flex-col-reverse">
-                      <button
-                        aria-label="Cancel"
-                        className="bg-figma-red text-figma-white desktop:min-w-[45%]  tablet:min-w-[8rem] tablet:max-w-[15rem] h-[3rem] phone:min-w-[10rem] hover:bg-figma-white hover:text-figma-dark-blue duration-700 rounded-[0.375rem] figma-body-m mb-[1.5rem]"
-                        type="button"
+          {({ onClose }) => (
+            <div className="relative">
+              <div className="fixed inset-0 bg-black z-40 bg-opacity-50 flex justify-center items-center">
+                <div className="desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] ">
+                  <div className="bg-figma-semi-dark-blue text-figma-white rounded-t-3xl rounded-b-3xl rounded shadow-2xl shadow-figma-red ">
+                    <div className="relative">
+                      <img
+                        src={
+                          show.thumbnail.regular.large.startsWith("/")
+                            ? show.thumbnail.regular.large
+                            : `/${show.thumbnail.regular.large}`
+                        }
+                        alt={show.title}
+                        className="desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] h-auto object-contain rounded-t-3xl border-2 "
+                      ></img>
+                      <div className="absolute top-2 left-20">
+                        <BookmarkButton
+                          media_id={show.id}
+                          isBookmarked={show.isBookmarked}
+                          reloadData={fetchData}
+                        />
+                      </div>
+                      <div
+                        className="absolute top-[1.60rem] right-8"
                         onClick={onClose}
                       >
-                        Cancel
-                      </button>
-                      <button
-                        aria-label="Watch now"
-                        className="bg-figma-red text-figma-white desktop:min-w-[45%]  tablet:min-w-[8rem] tablet:max-w-[15rem] h-[3rem] phone:min-w-[10rem] hover:bg-figma-white hover:text-figma-dark-blue duration-700 rounded-[0.375rem] figma-body-m mb-[1.5rem]"
-                        type="button"
-                        onClick={onLoginCheck}
-                      >
-                        Watch Now
-                      </button>
+                        <CloseDescription />
+                      </div>
+                    </div>
+                    <p className="text-center figma-heading-l border-x-2 pt-[1rem] break-words">
+                      {show.title}
+                    </p>
+                    <div className="grid grid-cols-3 place-items-center desktop:px-[3rem] tablet:px-[2rem]  phone:px-[1rem] figma-heading-m pt-[1rem] pb-[1rem] border-x-2 w-[30vw] desktop:w-[40rem] tablet:min-w-[30rem]  phone:min-w-[20rem]">
+                      <div className="flex flex-col text-center desktop:min-w-[40rem] tablet:min-w-[30rem] phone:min-w-[20rem] w-[30vw] ">
+                        <p className="figma-body-s  phone:text-[0.5em]">
+                          Release Year:
+                        </p>
+                        <p>{show.year}</p>
+                      </div>
+                      <div className="flex flex-col text-center ">
+                        <p className="figma-body-s phone:text-[0.5em]">
+                          Category:
+                        </p>
+                        <p className="">
+                          {show.category === "TV Series" ? "Series" : "Movie"}
+                        </p>
+                      </div>
+                      <div className="flex flex-col text-center">
+                        <p className="figma-body-s phone:text-[0.5em]">
+                          Rating:
+                        </p>
+                        <p>{show.rating}</p>
+                      </div>
+                    </div>
+                    <div className="flex justify-center pb-3 border-x-2">
+                      <RatingsButton
+                        showID={showID}
+                        contentId={show.id}
+                        averageRating={show.averageRating}
+                        totalRatings={show.totalRatings}
+                        userRating={show.userRating}
+                      />
+                    </div>
+                    <div className="flex flex-col text-center desktop:w-[40rem] tablet:min-w-[30rem]  phone:min-w-[20rem] w-[30vw] px-[2rem] figma-body-m border-x-2 border-b-2 rounded-b-3xl">
+                      <p>Synopsis:</p>
+                      <p className="">{show.description}</p>
+                      <div className="flex pt-[3rem] justify-between px-[3rem] gap-x-[3rem] tablet:flex-row phone:flex-col-reverse">
+                        <button
+                          aria-label="Cancel"
+                          className="bg-figma-red text-figma-white desktop:min-w-[45%]  tablet:min-w-[8rem] tablet:max-w-[15rem] h-[3rem] phone:min-w-[10rem] hover:bg-figma-white hover:text-figma-dark-blue duration-700 rounded-[0.375rem] figma-body-m mb-[1.5rem]"
+                          type="button"
+                          onClick={onClose}
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          aria-label="Watch now"
+                          className="bg-figma-red text-figma-white desktop:min-w-[45%]  tablet:min-w-[8rem] tablet:max-w-[15rem] h-[3rem] phone:min-w-[10rem] hover:bg-figma-white hover:text-figma-dark-blue duration-700 rounded-[0.375rem] figma-body-m mb-[1.5rem]"
+                          type="button"
+                          onClick={onLoginCheck}
+                        >
+                          Watch Now
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
           )}
         </AnimationRouteModal>
       )}
